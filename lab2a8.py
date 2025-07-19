1a602003d24a34d4d8b22cb4515a398c9594bd3f
nwordabuser <153253665+nwordabuser@users.noreply.github.com>
2025-07-19 18:06:51 +0530

    lab question 8

---
+++ lab2a8.py
@@ -0,0 +1,15 @@
+import numpy as np  ## for matrices 
+import pandas as pd ## for importing xlxs  
+
+## loading the required sheet 
+file_path = "/Users/niteshnirranjan/Downloads/Lab Session Data.xlsx"
+df = pd.read_excel(file_path, sheet_name='thyroid0387_UCI')
+
+## making "?" as nan
+df.replace("?", np.nan, inplace=True)
+df["TSH"] = pd.to_numeric(df["TSH"], errors='coerce')
+
+## filling in missing values for income 
+df["TSH"].fillna(df["TSH"].median(), inplace=True)
+print(df["TSH"])
+
